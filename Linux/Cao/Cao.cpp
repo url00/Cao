0bf37d11e10df1775a58d681cd7eb492e59a688a
Nathan Bergeron <nathanbrgrn@gmail.com>
2017-02-15 21:28:14 -0600

    Minor refactoring.

--- Linux/Cao/Cao.cpp
+++ Linux/Cao/Cao.cpp
@@ -4,24 +4,27 @@
 #include <gtk/gtk.h>
 #include "Cross.h"
 
+static GApplication *app;
+static GtkApplication *app_gtk;
+
 static void
 activate (
-    GtkApplication* app,
+    GtkApplication* __app,
     gpointer        user_data)
 {
-    srand(time(NULL));
-    for (int i = 0; i < 40; i++)
-    {
-        GtkWidget *window_widget;
+    GtkWidget *window_widget;
+    window_widget = gtk_application_window_new(app_gtk);
+    GtkWindow *window = (GtkWindow*)window_widget;
+    gtk_window_set_title(window, "Cao");
+    gtk_window_set_default_size(window, 500, 50);
+    gtk_widget_show_all(window_widget);
 
-        window_widget = gtk_application_window_new(app);
-        GtkWindow *window = (GtkWindow*)window_widget;
 
-        gtk_window_set_title(window, "Cao");
-        gtk_window_set_default_size(window, 10 + rand() % 200, 10 + rand() % 200);
-        gtk_window_move(window, (i * 2) + rand() % 600, (i * 2) + rand() % 600);
-        gtk_widget_show_all(window_widget);
-    }
+
+    GNotification *notification;
+    notification = g_notification_new ("Lunch is ready");
+    g_notification_set_body (notification, "Today we have pancakes and salad, and fruit and cake for dessert");
+    g_application_send_notification(app, "test", notification);
 }
 
 int
@@ -29,15 +32,16 @@ main (
     int    argc,
     char **argv)
 {
-    GtkApplication *app;
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    app_gtk = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    app = (GApplication*)app_gtk;
 
-    g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
+    g_signal_connect(app_gtk, "activate", G_CALLBACK(activate), NULL);
+
+
 
     int status;
-    status = g_application_run(G_APPLICATION(app), argc, argv);
+    status = g_application_run(app, argc, argv);
 
     g_object_unref (app);
-
     return status;
 }
