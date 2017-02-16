fdf9a6ec91f435b8e9a93cf6adf174a299ff8e44
Nathan Bergeron <nathanbrgrn@gmail.com>
2017-02-15 21:06:27 -0600

    Cleaned up some of the build.

--- Linux/Cao/Cao.cpp
+++ Linux/Cao/Cao.cpp
@@ -1,32 +1,35 @@
 #include "Cao.h"
 
 #include <gtk/gtk.h>
+#include "Cross.h"
 
 static void
 activate (
-  GtkApplication* app,
-  gpointer        user_data)
+    GtkApplication* app,
+    gpointer        user_data)
 {
-  GtkWidget *window;
+    GtkWidget *window;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-  gtk_widget_show_all (window);
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Cao");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_widget_show_all (window);
 }
 
 int
 main (
-  int    argc,
-  char **argv)
+    int    argc,
+    char **argv)
 {
-  GtkApplication *app;
-  int status;
+    GtkApplication *app;
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 
-  return status;
+    int status;
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    g_object_unref (app);
+
+    return status;
 }
